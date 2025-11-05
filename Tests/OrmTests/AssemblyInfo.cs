6d93facc65bf4e5d1859f0d1ccd423fda2db0aa4
Temo Nikolaishvili <t.nikolaishvili@altasoft.ge>
2025-11-05 12:58:32 +0400

    MC

--- Tests/OrmTests/AssemblyInfo.cs
+++ Tests/OrmTests/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 //Optional
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
 //Optional
-[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
+//[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
 //Optional
 //[assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
